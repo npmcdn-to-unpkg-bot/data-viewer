752b1cc6e0283874fcf30b60764cb0b135d9f9a7
Antti Rauhala <anttirauhala@users.noreply.github.com>
2016-08-13 18:17:15 +0300

    Styles for pages

--- routes/index_routes.js
+++ routes/index_routes.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var pagemetadata =  {
-	pagetitle : 'Avoimen datan palvelut'
-}
-	
-var functiontitle_start = 'Aloitus'
-
 /* GET inex.html page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { pagemetadata: pagemetadata, functiontitle : functiontitle_start });
+  res.render('index');
 });
 
 module.exports = router;
